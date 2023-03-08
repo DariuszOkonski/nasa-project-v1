1f8448278b29a2304b9f597c7b9149ed79cfe08c
Dariusz Okoński <darek200180@gmail.com>
2023-03-08 18:04:39 +0100

    add some commentds

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,7 +11,14 @@ app.use(
     origin: 'http://localhost:3000',
   })
 );
+
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.use(planetsRouter);
 
